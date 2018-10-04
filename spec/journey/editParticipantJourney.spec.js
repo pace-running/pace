871f516bf6d1e42a60602064357b1bef99587f07
Christoph <chris@inferno.nadir.org>
2018-10-04 16:26:50 +0200

    migrate editParticipant journey

---
+++ spec/journey/editParticipantJourney.spec.js
@@ -0,0 +1,15 @@
+context('participant edits themself', () => {
+  it('shows an error when using invalid link', () => {
+    cy.visit('/editparticipant/invalidSecureId',{failOnStatusCode: false})
+      .get('h1')
+      .should('contain','Teilnehmer nicht bekannt')
+  });
+  it('should edit a participant', () => {
+    cy.task('resetDb').then( () => {
+      cy.task('validUser');
+    });
+    cy.visit('/editparticipant/secureIdForTheEditLink')
+      .get('input#firstname')
+      .should('exist')
+  });
+});
