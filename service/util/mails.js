6bbe3c810b2a2d45b8328f7c78c614de999cc718
Christoph <chris@inferno.nadir.org>
2019-01-13 23:09:46 +0100

    we don't use sendmail anymore

--- service/util/mails.js
+++ service/util/mails.js
@@ -2,7 +2,6 @@
 'use strict';
 const _ = require('lodash');
 const nodemailer = require('nodemailer');
-const sendmailTransport = require('nodemailer-sendmail-transport');
 const config = require('config');
 const moment = require('moment');
 const pug = require('pug');
