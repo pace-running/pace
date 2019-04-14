94eed9a906940e7b997b81967bd0689af8836cd6
Christoph <chris@inferno.nadir.org>
2019-04-14 19:08:22 +0200

    Revert "Startnumbers for this years stadtpark-marathon"
    This reverts commit dab25ca218fc7d411dabaf18858fd003d12bb6af.
    
    very clean startnumber for now ...

--- pace-pdf/pdf/pdfGeneration.js
+++ pace-pdf/pdf/pdfGeneration.js
@@ -7,7 +7,6 @@ const config = require('config');
 var Archiver = require('archiver');
 
 const qr = require('qr-image');
-const barcode = require("rescode");
 
 let pdfGeneration = {};
 
@@ -19,6 +18,16 @@ let createOutputDir = () => {
 };
 
 const pathToBackgroundImage = '/images/background_light.jpg';
+const pathToLogoLeft = '/images/lauf_gegen_rechts_logo.jpg';
+const pathToLogoRight = '/images/fc_st_pauli_marathon_logo.png';
+const checkmarkSymbolSvg = 'M7.375,25 c0,0,10,11.375,14.125,11.375S44.875,8,44.875,8';
+
+pdfGeneration.addCheckmarkSymbol = (doc) => {
+  doc.translate(20, 280)
+    .path(checkmarkSymbolSvg)
+    .lineWidth(3)
+    .stroke();
+};
 
 pdfGeneration.addQrCodeWithSelfServiceLink = (doc, selfServiceUrl) => {
   doc.fontSize(10).fillColor('black').text('Registriere dich', 300, 320);
@@ -43,18 +52,24 @@ pdfGeneration.generate = (startNumberData) => {
 
 pdfGeneration.createStartNumberPage = (doc, startNumberData) => {
 
-  doc.image(__dirname + pathToBackgroundImage, {width: 600,height: 420});
+  doc.image(__dirname + pathToBackgroundImage, {fit: [800, 800]});
+  //  doc.image(__dirname + pathToLogoLeft, 20, 20, {fit: [100, 100]});
+  doc.image(__dirname + pathToLogoRight, 475, 20, {fit: [100, 100]});
 
   doc.font('Helvetica-Bold').fontSize(200).fillColor('saddlebrown').text(startNumberData.startNumber, 0, 130, {align: 'center'});
-  doc.fontSize(30).fillColor('red').text(startNumberData.team.substring(0, 25), 0, 300, {align: 'center'});
+  doc.fontSize(40).fillColor('red').text(startNumberData.firstname.substring(0, 17), 0, 300, {align: 'center'});
+  doc.fontSize(30).fillColor('red').text(startNumberData.team.substring(0, 25), 0, 350, {align: 'center'});
+  console.log(startNumberData.startBlockColor);
 
-  barcode.loadModules(["code128"], {'includetext': false, 'scaleX': 2});
-  let barcodeSvg = barcode.create("code128", String(startNumberData.startNumber));
+  doc.fontSize(35).fillColor(startNumberData.startBlockColor).text('Startblock: '+startNumberData.startBlock, 200, 20, {align: 'left'});
 
-  doc.image(barcodeSvg, 20, 230, {fit: [70, 70]});
-  doc.image(barcodeSvg, 20, 250, {fit: [70, 70]});
-  doc.image(barcodeSvg, 500, 230, {fit: [70, 70]});
-  doc.image(barcodeSvg, 500, 250, {fit: [70, 70]});
+  if(startNumberData.tshirt) {
+    doc.fontSize(12).fillColor('black').text(startNumberData.tshirt.size + ' ' + startNumberData.tshirt.model, 500, 315);
+  }
+
+  if(startNumberData.hasPayed) {
+    pdfGeneration.addCheckmarkSymbol(doc);
+  }
 
   if(startNumberData.onSiteRegistration) {
     pdfGeneration.addQrCodeWithSelfServiceLink(doc, startNumberData.secureUrl);
