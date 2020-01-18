04384460deb7f519c1ccb44276ac1f17cf7e8cd8
Christoph <chris@inferno.nadir.org>
2020-01-18 19:59:37 +0100

    use kustomize to build valid config
    "kubectl -k" and kustomize differ in what they expect ...

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -20,13 +20,10 @@ then
   docker tag pace_pace-pdf pacerunning/pace-pdf:latest
   docker tag pace_pace-pdf pacerunning/pace-pdf:$TRAVIS_COMMIT
   docker push pacerunning/pace-pdf
-  echo "writing secrets:"
   echo "DATASE_URL=$DATABASE_URL_DEV" > k8s/base/secrets.env
   echo "REDIS_URL=$REDIS_URL" >> k8s/base/secrets.env
-  echo "secrets written:"
-  cat k8s/base/secrets.env
   echo "running kustomize"
   cd k8s/base && kustomize edit set image "pacerunning/pace-app=pacerunning/pace-app:$TRAVIS_COMMIT" 
-  cd .. && kubectl --token $KUBE_TOKEN apply --namespace dev -k overlays/dev   
-  kubectl kustomize overlays/dev 
+  cd .. && kustomize build overlays/dev | kubectl --token $KUBE_TOKEN apply --namespace dev -f - 
+  kustomize build overlays/dev 
 fi
