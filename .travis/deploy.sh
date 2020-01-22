311c6f90c8bd6103cf34a5e7f1ceb0242cfa30df
Christoph <chris@inferno.nadir.org>
2020-01-22 20:47:51 +0100

    [k8s] 'set -e' does not work in if statements?

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,24 +6,24 @@ set -u # Disallow unset variables
 
 
 # Only run when not part of a pull request and on the master branch
-if [ $TRAVIS_BRANCH = "master" ]
-then
-  set -e
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-  chmod 755 ./kubectl
-  export PATH=$PWD:$PWD/.travis/:$PATH
-  export KUBECONFIG=$(pwd)/.travis/kubeconfig 
-
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker tag pace_pace-app pacerunning/pace-app:latest
-  docker tag pace_pace-app pacerunning/pace-app:$TRAVIS_COMMIT
-  docker push pacerunning/pace-app
-  docker tag pace_pace-pdf pacerunning/pace-pdf:latest
-  docker tag pace_pace-pdf pacerunning/pace-pdf:$TRAVIS_COMMIT
-  docker push pacerunning/pace-pdf
-  echo "DATABASE_URL=$DATABASE_URL_DEV" > k8s/base/secrets.env
-  echo "REDIS_URL=$REDIS_URL" >> k8s/base/secrets.env
-  cd k8s/base && kustomize edit set image "pacerunning/pace-app=pacerunning/pace-app:$TRAVIS_COMMIT" 
-  cd .. && kustomize build overlays/dev | kubectl --token $KUBE_TOKEN apply --namespace dev -f - 
-  kubectl rollout status deployment dev-pace-app-deployment --timeout=30s -w 
+if [ $TRAVIS_BRANCH != "master" ]; then
+  exit 1
 fi
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod 755 ./kubectl
+export PATH=$PWD:$PWD/.travis/:$PATH
+export KUBECONFIG=$(pwd)/.travis/kubeconfig 
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker tag pace_pace-app pacerunning/pace-app:latest
+docker tag pace_pace-app pacerunning/pace-app:$TRAVIS_COMMIT
+docker push pacerunning/pace-app
+docker tag pace_pace-pdf pacerunning/pace-pdf:latest
+docker tag pace_pace-pdf pacerunning/pace-pdf:$TRAVIS_COMMIT
+docker push pacerunning/pace-pdf
+echo "DATABASE_URL=$DATABASE_URL_DEV" > k8s/base/secrets.env
+echo "REDIS_URL=$REDIS_URL" >> k8s/base/secrets.env
+cd k8s/base && kustomize edit set image "pacerunning/pace-app=pacerunning/pace-app:$TRAVIS_COMMIT" 
+cd .. && kustomize build overlays/dev | kubectl --token $KUBE_TOKEN apply --namespace dev -f - 
+kubectl rollout status deployment dev-pace-app-deployment --timeout=30s -w 
