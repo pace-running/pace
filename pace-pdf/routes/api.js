4f2536d6f6c8e364c4ee4b4bd87cd84ad556b457
Christoph <chris@inferno.nadir.org>
2020-05-13 21:33:32 +0200

    endpoint for downloading a single pdf

--- pace-pdf/routes/api.js
+++ pace-pdf/routes/api.js
@@ -4,6 +4,7 @@
 const router = require('express').Router();
 const _ = require('lodash');
 const pdfGeneration = require('../pdf/pdfGeneration');
+const config = require('config');
 
 router.get('/startnumbers', (req, res) => {
   res.setHeader('Content-Disposition', 'attachment');
@@ -13,3 +14,11 @@ router.get('/startnumbers', (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/startnumber/:number', (req,res) => {
+  res.setHeader('Content-Disposition', 'attachment');
+  res.setHeader('Content-disposition', 'attachment; filename=startnumber.pdf');
+  res.setHeader('Content-type', 'application/pdf');
+  let filepath = config.get('pdfPath') + req.params.number + '.pdf';
+  res.download(filepath)
+})
